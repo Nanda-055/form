5309affbd5bdcfc6b291393ddf5f5bfa6f8f53e4
Nanda Prasetya <61816193+Nanda-055@users.noreply.github.com>
2021-03-01 21:23:36 +0700

    Add files via upload

---
+++ actionget.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Get</title>
+</head>
+<body>
+	Welcome <?php echo $_GET["name"]; ?><br>
+	Your email address is: <?php echo $_GET["email"]; ?>
+</body>
+</html>
+
